9d9976a2dd370bd26d5cd8897b5da8853d5ddb19
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-04-07 23:13:05 -0400

    installs keybindings for openbox .xml

--- install.sh
+++ install.sh
@@ -9,11 +9,20 @@ g++ -o configure-theme configure-theme.cpp
 # Will pass user path to help C program find files easier
 userPath=`echo /home/$USER`
 
-# Temporary buffer for the data being appended to the final file  
+# Store the data to be appended in the $HOME dir
 cp appendData $userPath
 
 # Run auto append program, passing in user pathname
 ./configure-theme $userPath
+
+# Install keybindings to get the gaps/margins: 
+javac -d . initKeybindings.java
+
+echo Type name of ~/.config/openbox/~.xml file. Example: lxde-rc.xml, or rc.xml
+read userConfig 
+
+# Pass in filepath for user rc.xml filename, and filepath for bindings being added
+java insertion.initKeybindings $userPath/.config/openbox/$userConfig $userPath/build/pseudo-border-color-margins/bindings.xml
 
 # Cleanup:
 rm $userPath/appendData
